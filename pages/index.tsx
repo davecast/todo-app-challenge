c11a0ceca38b5306813987f35f49bebcde8f634e
David Castillo <hola@davecastworks.com>
2021-04-07 01:34:02 -0400

    [update] add initials state

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,28 @@
-import ToDoForm from '../components/ToDoForm'
-import ToDoList from "../components/ToDoList"
+import { useState } from 'react'
 
+import ToDoForm from '../components/ToDoForm'
+import ToDoList from '../components/ToDoList'
 
 const Home = () => {
+  const [tasks, setTasks] = useState([])
+  const [task, setTask] = useState<string>('')
+  const [loadingTasks, setLoadingTasks] = useState(false)
+
   return (
     <div>
       <h1>To do system</h1>
-      <ToDoForm />
-      <ToDoList />
+      <ToDoForm
+        task={task}
+        setTask={setTask}
+        tasks={tasks}
+        setTasks={setTasks}
+      />
+      <ToDoList
+        tasks={tasks}
+        setTasks={setTasks}
+        loadingTasks={loadingTasks}
+        setLoadingTasks={setLoadingTasks}
+      />
     </div>
   )
 }
