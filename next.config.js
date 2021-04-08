71adb66e432ad8e2127ef3fce45a7d446c95f8c0
David Castillo <hola@davecastworks.com>
2021-04-07 22:40:18 -0400

    [fix] modify next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
+const path = require('path')
+
 module.exports = {
   future: {
     webpack5: true,
+  },
+  target: 'serverless',
+  webpack(config) {
+    config.resolve.modules.push(path.resolve('./'))
+    return config
   },
 }
